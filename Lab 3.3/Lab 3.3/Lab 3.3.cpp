0041e4da33ab012888b77bb9db7d4d5b90d3dccc
KovalDE <72768859+KovalDE@users.noreply.github.com>
2020-10-24 22:14:59 +0300

    Add files via upload

---
+++ Lab 3.3/Lab 3.3/Lab 3.3.cpp
@@ -0,0 +1,33 @@
+﻿// Lab_03_3.cpp
+// < Коваля Дмитра >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 14
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double x; 
+	double y; 
+	cout << "x = "; cin >> x;
+
+	if (x <= -4)
+		y = -2;
+	else
+		if (-4 < x && x <= 0)
+			y = -1 + ((1./ 4) * (x + 4));
+		else
+			if (x > 0 && x <= 2)
+				y = x * x;
+			else
+			y = 4 + ((-4./ 8) * (x - 2));
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+
+}
+
+
